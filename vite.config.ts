f9f4d8112f211093c5d32c7fea0dd5b4275dda66
tehehe <101838111+ttvbenjoxx@users.noreply.github.com>
2024-12-06 12:06:56 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tantler-Enhancer/', // Your GitHub repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
